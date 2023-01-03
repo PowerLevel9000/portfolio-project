a57524d292a56aa53146b932bc0a05c0c40fcc1f
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-03 10:27:08 +0530

    array load fix navbar

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 const navBtn = document.querySelector('nav button');
 const overlay = document.querySelector('.desk');
 const navList = document.querySelectorAll('.content a');
-const menuButton = document.querySelector('#menu')
+const menuButton = document.querySelector('#menu');
 
-// console.log(navList)
 navBtn.addEventListener('click', () => {
-  overlay.classList.toggle('desk1')
-  for (let a of navList) {
-    a.addEventListener('click', () => {
-      if (overlay.classList.value == 'desk desk1') {
-        overlay.classList.value = 'desk';
-        if (menuButton.classList.value == 'fa-solid fa-bars') {
-          menuButton.classList.value = 'fa-solid fa-xmark'
-        } else if (menuButton.classList.value == 'fa-solid fa-xmark') {
-          menuButton.classList.value = 'fa-solid fa-bars'
-        }
-      }
-    })
-  }
-  if (menuButton.classList.value == 'fa-solid fa-bars') {
-    menuButton.classList.value = 'fa-solid fa-xmark'
-  } else if (menuButton.classList.value == 'fa-solid fa-xmark') {
-    menuButton.classList.value = 'fa-solid fa-bars'
-  }
+  overlay.classList.toggle('desk1');
+  menuButton.classList.toggle('fa-bars');
+  menuButton.classList.toggle('fa-xmark');
 });
+for (const a of navList) {
+  a.addEventListener('click', () => {
+    if (overlay.classList.value === 'desk desk1') {
+      overlay.classList.value = 'desk';
+      menuButton.classList.value = 'fa-solid fa-bars';
+    }
+  });
+}
