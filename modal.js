1e5091eb90ee01f3d9f53d2c582cdbd65338bc17
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-04 07:37:51 +0530

    model object created

---
+++ modal.js
@@ -0,0 +1,88 @@
+const card1 = {
+  title: 'Introduction Form',
+  projectMaker: [
+    'Adarsh',
+    'Future Fullstack',
+    '2022'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essent'`,
+  techList: [
+    'HTML',
+    'CSS'
+  ],
+  link: [
+    'https://PowerLevel9000.github.io/Introduction-Form/',
+    'https://github.com/PowerLevel9000/Introduction-Form/',
+  ],
+  image: 'projects-img/introduction-form.png',
+};
+
+const card2 = {
+  title: 'Tutorial',
+  projectMaker: [
+    'Adarsh',
+    'Future Fullstack',
+    '2022'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essent'`,
+  techList: [
+    'HTML',
+    'CSS'
+  ],
+  link: [
+    'https://Powerlevel9000.github.io/DocumentationProject/DocumentationProject.html',
+    'https://github.com/PowerLevel9000/DocumentationProject',
+  ],
+  image: 'projects-img/cyber-security.png',
+};
+
+const card3 = {
+  title: `Bhardvaj's tribute`,
+  projectMaker: [
+    'Adarsh',
+    'Future Fullstack',
+    '2022'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essent'`,
+  techList: [
+    'HTML',
+    'CSS'
+  ],
+  link: [
+    'https://codepen.io/PowerLevel9000/pen/ExReWZm',
+    'https://codepen.io/PowerLevel9000/pen/ExReWZm',
+  ],
+  image: 'projects-img/tribute-project.png',
+};
+
+const card4 = {
+  title: 'Penguin',
+  projectMaker: [
+    'Adarsh',
+    'Future Fullstack',
+    '2022'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essent'`,
+  techList: [
+    'HTML',
+    'CSS'
+  ],
+  link: [
+    'https://PowerLevel9000.github.io/Panguin-animation/',
+    'https://github.com/PowerLevel9000/Panguin-animation/',
+  ],
+  image: 'projects-img/css-animation-1.png',
+  iframe: 'https://PowerLevel9000.github.io/Panguin-animation/'
+};
