da4fe3f03b46f5d828bcc1630606f4fd06bb807e
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-04 09:22:08 +0530

    add desktop popup styling

--- modal.js
+++ modal.js
@@ -77,7 +77,8 @@ const card4 = {
     typesetting, remaining essent'`,
   techList: [
     'HTML',
-    'CSS'
+    'CSS',
+    'javascript'
   ],
   link: [
     'https://PowerLevel9000.github.io/Panguin-animation/',
@@ -91,6 +92,42 @@ const projects = [card1, card2, card3, card4];
 function modal(number) {
   const card = document.querySelector('#card');
   card.classList.toggle('invisible');
+  const title = document.querySelector('#card-title');
+  const list = document.querySelector('#card-list');
+  list.innerHTML = '';
+  const img = document.querySelector('.card-image');
+  const iframe = document.querySelector('.card-iframe')
+  const desc = document.querySelector('#card-description');
+  const techList = document.querySelector('#tech-list');
+  techList.innerHTML = ''
+  const buttonsrc = document.querySelector('#card-button-source');
+  const buttonlive = document.querySelector('#card-button-live');
+
+  title.innerHTML = projects[number].title;
+
+  for (let i = 0; i < projects[number].projectMaker.length; i ++) {
+    const li = document.createElement('li');
+    li.textContent = projects[number].projectMaker[i];
+    list.appendChild(li);
+  }
+  if(number===3){
+    img.classList.toggle('invisible');
+    iframe.classList.toggle('invisible');
+    iframe.setAttribute('src',projects[number].iframe);
+  }else{
+    iframe.classList.value='card-iframe invisible';
+    img.classList.value='card-image'
+    img.setAttribute('src', projects[number].image);
+  }
+
+  for(let i = 0; i < projects[number].techList.length; i++){
+    const li = document.createElement('li');
+    li.textContent = projects[number].techList[i];
+    techList.appendChild(li)
+  }  
+  desc.innerHTML = projects[number].description;
+  buttonsrc.setAttribute('onclick', `window.open("${projects[number].link[1]}", "_blank");`);
+  buttonlive.setAttribute('onclick', `window.open("${projects[number].link[0]}", "_blank");`);
 }
 function toggle() {
   const card = document.querySelector('#card');
