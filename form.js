296198824c7ea0055464ac62e8f2f5d0546323cd
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-05 21:04:16 +0530

    linter updated for better workflow

--- form.js
+++ form.js
@@ -1,28 +1,24 @@
 const form = document.getElementById('form');
-const name = form.elements['name']
-const email = form.elements['email'];
-const message = form.elements['message'];
+const { name } = form.elements;
+const { email } = form.elements;
+const { message } = form.elements;
 const submit = document.getElementById('submit');
 const error = document.getElementById('alert');
 error.innerHTml = '';
-console.log(form, submit, name, email, message, submit);
 submit.addEventListener('click', () => {
   form.addEventListener('submit', (e) => {
-    if(name.value.length <= 3){
+    if (name.value.length <= 3) {
       error.innerHTML = 'enter the full name';
       e.preventDefault();
-    }else if(email.value !== email.value.toLowerCase() || email.value.length < 1 || email.value === undefined) {
+    } else if (email.value !== email.value.toLowerCase() || email.value.length < 1) {
       e.preventDefault();
       error.innerHTML = 'email field has to be in lower case';
-    }else if(message.value.length < 5 || message.value === undefined ) {
-      e.preventDefault()
+    } else if (message.value.length < 5 || message.value === undefined) {
+      e.preventDefault();
       error.innerHTML = 'your message is valuable more words please';
-    }else {
+    } else {
       error.innerHTML = '';
-      form.submit()
+      form.submit();
     }
   });
-})
-
-
-
+});
