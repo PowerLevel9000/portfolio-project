95f094d6907b96dd955b3cb3bc6b7850c7e2e86d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-05 08:42:09 +0530

    thermodynamics by javaScript for project

--- modal.js
+++ modal.js
@@ -8,7 +8,7 @@ const card1 = {
   description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
     make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essent'`,
+    typesetting, remaining essential'`,
   techList: [
     'HTML',
     'CSS',
@@ -18,6 +18,13 @@ const card1 = {
     'https://github.com/PowerLevel9000/Introduction-Form/',
   ],
   image: 'projects-img/introduction-form.png',
+  alt: 'introduction-form',
+  lilDescription: `This project good to visit to start conversations
+  with me; no accounts or sign-ups required don't worry about password .`,
+  animation: [
+    'right',
+    'left'
+  ],
 };
 
 const card2 = {
@@ -30,7 +37,7 @@ const card2 = {
   description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
     make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essent'`,
+    typesetting, remaining essential'`,
   techList: [
     'HTML',
     'CSS',
@@ -40,6 +47,13 @@ const card2 = {
     'https://github.com/PowerLevel9000/DocumentationProject',
   ],
   image: 'projects-img/cyber-security.png',
+  alt: 'cyber-security',
+  lilDescription: `This is my cyber-security Document, I have made this with the template
+  of freeCodeCamp documentation project.`,
+  animation: [
+    'left',
+    'right',
+  ],
 };
 
 const card3 = {
@@ -52,7 +66,7 @@ const card3 = {
   description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
     make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essent'`,
+    typesetting, remaining essential'`,
   techList: [
     'HTML',
     'CSS',
@@ -62,6 +76,13 @@ const card3 = {
     'https://codepen.io/PowerLevel9000/pen/ExReWZm',
   ],
   image: 'projects-img/tribute-project.png',
+  alt: 'tribute-project',
+  lilDescription: `This is a tribute page for indian Bhardvaj rishi who is the father of guru of
+  of guru of Pandavas and made many researches.`,
+  animation: [
+    'right',
+    'left',
+  ],
 };
 
 const card4 = {
@@ -74,7 +95,7 @@ const card4 = {
   description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
     make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essent'`,
+    typesetting, remaining essential'`,
   techList: [
     'HTML',
     'CSS',
@@ -85,41 +106,115 @@ const card4 = {
     'https://github.com/PowerLevel9000/Panguin-animation/',
   ],
   image: 'projects-img/css-animation-1.png',
+  alt: 'Penguin Animation',
   iframe: 'https://PowerLevel9000.github.io/Panguin-animation/',
+  lilDescription: `I made this penguin css-animation when i was learning css month ago with free code camp
+  and i love this project do checkout`,
+  animation: [
+    'left', 'right',
+  ],
 };
 
 const projects = [card1, card2, card3, card4];
+
+for (let i = 0; i < projects.length; i += 1) {
+  const project = document.getElementById('project');
+  const projectCard = document.createElement('div');
+  projectCard.className = 'project one';
+
+  if (i === 3) {
+    projectCard.innerHTML = `
+         <iframe title="Project" src="${projects[3].iframe}" frameborder="0"
+          data-aos="fade-${projects[i].animation[0]}" data-aos-duration="1500"></iframe>
+        <div data-aos="fade-${projects[i].animation[1]}" data-aos-duration="1500">
+          <h2 title="Project Name" class="project-title">${projects[i].title}</h2>
+          <ul title="Project Maker" class="project-maker">
+            <li title="My Name" class="project-owner">${projects[i].projectMaker[0]}</li>
+            <li>${projects[i].projectMaker[1]}</li>
+            <li>${projects[i].projectMaker[2]}</li>
+          </ul>
+          <p title="Project Info" class="description info">
+          ${projects[i].lilDescription}
+          </p>
+          <ul title="Tech Used" class="tech">
+            <li>Html</li>
+            <li>Css</li>
+            <li>JavaScript</li>
+          </ul>
+          <div class=buttonLink onclick="modal(${i})">
+            <button>See Project</button>
+          </div>
+        </div>
+`
+  } else {
+    projectCard.innerHTML = `
+        <img title="Project" src="${projects[i].image}" alt="${projects[i].alt}" data-aos="fade-${projects[i].animation[0]}"
+          data-aos-duration="1500">
+        <div data-aos="fade-${projects[i].animation[1]}" data-aos-duration="1500">
+          <h2 title="Project Name" class="project-title">${projects[i].title}</h2>
+          <ul title="Project Maker" class="project-maker">
+            <li title="My Name" class="project-owner">${projects[i].projectMaker[0]}</li>
+            <li>${projects[i].projectMaker[1]}</li>
+            <li>${projects[i].projectMaker[2]}</li>
+          </ul>
+          <p title="Project Info" class="description info">
+          ${projects[i].lilDescription}
+          </p>
+          <ul title="Tech Used" class="tech">
+            <li>Html</li>
+            <li>Css</li>
+          </ul>
+          <div class=buttonLink onclick="modal(${i})">
+            <button>See Project</button>
+          </div>
+        </div>
+`
+  };
+
+  project.appendChild(projectCard);
+};
+
+
+
 function modal(number) {
   const card = document.querySelector('#card');
   card.classList.toggle('invisible');
   const title = document.querySelector('#card-title');
   const list = document.querySelector('#card-list');
   list.innerHTML = '';
+
   const img = document.querySelector('.card-image');
   const desc = document.querySelector('#card-description');
   const techList = document.querySelector('#tech-list');
   techList.innerHTML = '';
-  const buttonsrc = document.querySelector('#card-button-source');
-  const buttonlive = document.querySelector('#card-button-live');
+
+  const buttonSrc = document.querySelector('#card-button-source');
+  const buttonLive = document.querySelector('#card-button-live');
+
   title.innerHTML = projects[number].title;
   for (let i = 0; i < projects[number].projectMaker.length; i += 1) {
     const li = document.createElement('li');
     li.textContent = projects[number].projectMaker[i];
     list.appendChild(li);
   }
+
   img.setAttribute('src', projects[number].image);
+
   for (let i = 0; i < projects[number].techList.length; i += 1) {
     const li = document.createElement('li');
     li.textContent = projects[number].techList[i];
     techList.appendChild(li);
   }
+
   desc.innerHTML = projects[number].description;
-  buttonsrc.setAttribute('onclick', `window.open("${projects[number].link[1]}", "_blank");`);
-  buttonlive.setAttribute('onclick', `window.open("${projects[number].link[0]}", "_blank");`);
+  buttonSrc.setAttribute('onclick', `window.open("${projects[number].link[1]}", "_blank");`);
+  buttonLive.setAttribute('onclick', `window.open("${projects[number].link[0]}", "_blank");`);
 }
+
 function toggle() {
   const card = document.querySelector('#card');
   card.classList.toggle('invisible');
 }
+
 toggle();
 modal();
