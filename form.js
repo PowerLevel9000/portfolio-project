a50cc2cd44d4b58a3d94fb51139c144a2eb41b9c
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-06 18:37:41 +0530

    form validation update

--- form.js
+++ form.js
@@ -1,11 +1,13 @@
 const form = document.getElementById('form');
-const { name } = form.elements;
-const { email } = form.elements;
-const { message } = form.elements;
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const message = document.getElementById('message')
 const submit = document.getElementById('submit');
 const error = document.getElementById('alert');
 error.innerHTml = '';
-submit.addEventListener('click', () => {
+
+
+function validate() {
   form.addEventListener('submit', (e) => {
     if (name.value.length <= 3) {
       error.innerHTML = 'enter the full name';
@@ -21,4 +23,18 @@ submit.addEventListener('click', () => {
       form.submit();
     }
   });
-});
+  if (name.value.length <= 3) {
+    name.value.length === 0 ? error.innerHTML = '' : error.innerHTML = 'enter the full name';
+  } else if (email.value !== email.value.toLowerCase() || email.value.length < 1) {
+    email.value.length === 0 ? error.innerHTML = '' : error.innerHTML = 'email field has to be in lower case';
+  } else if (message.value.length < 5 || message.value === undefined) {
+    message.value.length === 0 ? error.innerHTML = '' : error.innerHTML = 'your message is valuable more words please';
+  } else {
+    error.innerHTML = '';
+  }
+}
+
+name.addEventListener('input', validate);
+email.addEventListener('input', validate);
+message.addEventListener('input', validate);
+submit.addEventListener('click', validate);
