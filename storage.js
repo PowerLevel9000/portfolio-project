9f114c3f4bd8a1c6b0463bf3e6ae847237fe9f28
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-15 13:08:48 +0530

    enhancing code base of storage.js makeing suntion more dry

--- storage.js
+++ storage.js
@@ -1,28 +1,25 @@
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-const userComment = document.getElementById('message');
+const formFields = ['name', 'email', 'message'];
 const contactForm = document.getElementById('form');
 
 function storeForm() {
-  const myStorage = {
-    userName: userName.value,
-    userEmail: userEmail.value,
-    userComment: userComment.value,
-  };
+  const myStorage = {};
+  formFields.forEach(field => {
+    myStorage[field] = contactForm[field].value;
+  });
   localStorage.setItem('storage', JSON.stringify(myStorage));
 }
 
-userName.addEventListener('change', storeForm);
-userEmail.addEventListener('change', storeForm);
-userComment.addEventListener('change', storeForm);
+formFields.forEach(field => {
+  contactForm[field].addEventListener('change', storeForm);
+});
 
 function refresh() {
   if (localStorage.getItem('storage')) {
     const oldStorage = localStorage.getItem('storage');
     const newStorage = JSON.parse(oldStorage);
-    contactForm.name.value = newStorage.userName;
-    contactForm.email.value = newStorage.userEmail;
-    contactForm.message.value = newStorage.userComment;
+    formFields.forEach(field => {
+      contactForm[field].value = newStorage[field];
+    });
   }
 }
 
